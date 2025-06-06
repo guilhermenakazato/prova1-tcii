b69208a5154935f1f4a49ccbae4de1a6ddb4c42a
Felipe Jun <felipejun@outlook.com>
2025-06-05 22:21:23 -0300

    Bounds2 e Vec2

---
+++ Bounds2.h
@@ -0,0 +1,91 @@
+#ifndef __Bounds2_h
+#define __Bounds2_h
+
+#include "Vec2.h"
+
+namespace tcii::cg
+{ // begin namespace tcii::cg
+
+    template <size_t D, typename real> class Bounds;
+
+    template <typename real>
+    class Bounds<2, real> // Bounds2
+    {
+    public:
+        using vec2 = Vec2<real>; // Vec2
+
+        Bounds()
+        {
+            setEmpty();
+        }
+
+        auto& operator [](size_t i) const
+        {
+            assert(i >= 0 && i < 2); // Ajustado para 2 dimensões
+            return (&_p1)[i];
+        }
+
+        auto& min() const
+        {
+            return _p1;
+        }
+
+        auto& max() const
+        {
+            return _p2;
+        }
+
+        bool contains(const vec2& p) const //vec2
+        {
+            if (p.x < _p1.x || p.x > _p2.x)
+                return false;
+            if (p.y < _p1.y || p.y > _p2.y)
+                return false; // z removido
+            return true;
+        }
+
+        void setEmpty()
+        {
+            _p1.x = _p1.y = +std::numeric_limits<real>::max();
+            _p2.x = _p2.y = -std::numeric_limits<real>::max(); // z removido
+        }
+
+        void inflate(const vec2& p) //vec2
+        {
+            if (p.x < _p1.x)
+                _p1.x = p.x;
+            if (p.x > _p2.x)
+                _p2.x = p.x;
+            if (p.y < _p1.y)
+                _p1.y = p.y;
+            if (p.y > _p2.y)
+				_p2.y = p.y; // z removido
+        }
+
+        void inflate(const Bounds& bounds)
+        {
+            inflate(bounds._p1);
+            inflate(bounds._p2);
+        }
+
+    private:
+        vec2 _p1; //vec2
+        vec2 _p2; //vec2
+
+    }; // Bounds2
+
+    template <typename real> using Bounds2 = Bounds<2, real>; // Bounds2
+
+    template <typename real>
+    std::ostream&
+		operator <<(std::ostream& os, const Bounds2<real>& b) // Bounds2
+    {
+        os << "min" << b.min() << " max" << b.max();
+        return os;
+    }
+
+    using Bounds2f = Bounds2<float>; // Bounds2
+
+} // end namespace tcii::cg
+
+#endif // __Bounds2_h
---
+++ Vec2.h
@@ -0,0 +1,106 @@
+#ifndef __Vec2_h
+#define __Vec2_h
+
+#include <cassert>
+#include <cmath>
+#include <iostream>
+
+namespace tcii::cg
+{ // begin namespace tcii::cg
+
+#define ASSERT_REAL(T, msg) static_assert(std::floating_point<T>, msg)
+
+    template <size_t D, typename real> struct Vec;
+
+    template <typename real>
+    struct Vec<2, real> // Ajustado para 2 dimensões
+    {
+        ASSERT_REAL(real, "Vec2: floating-point type expected"); // Nome Vec2
+
+        using value_type = real;
+
+        real x;
+        real y; // Sem coordenada z
+
+        auto& operator [](size_t i)
+        {
+            assert(i >= 0 && i < 2); // Ajustado para i < 2
+            return (&x)[i];
+        }
+
+        auto operator [](size_t i) const
+        {
+            return const_cast<Vec*>(this)->operator [](i);
+        }
+
+        real length() const;
+
+        auto versor() const
+        {
+            return ((real)1 / length()) * *this;
+        }
+
+    }; // Nome Vec2 
+
+	template <typename real> using Vec2 = Vec<2, real>; // Ajustado para 2 dimensões
+
+    template <typename real>
+    inline Vec2<real> // Tipo Vec2
+        operator +(const Vec2<real>& u, const Vec2<real>& v) // Tipo Vec2
+    {
+        return { u.x + v.x, u.y + v.y}; // z removido
+    }
+
+    template <typename real>
+    inline bool
+		operator ==(const Vec2<real>& u, const Vec2<real>& v) // Tipo Vec2
+    {
+		for (size_t i = 0; i < 2; i++) { // Ajustado para 2 dimensões
+            if (u[i] != v[i])
+                return false;
+        }
+
+        return true;
+    }
+
+    template <typename real>
+	inline Vec2<real> // Tipo Vec2
+		operator -(const Vec2<real>& u, const Vec2<real>& v) // Tipo Vec2
+    {
+		return { u.x - v.x, u.y - v.y}; // z removido
+    }
+
+    template <typename real>
+	inline Vec2<real> // Tipo Vec2
+		operator *(real s, const Vec2<real>& v) // Tipo Vec2
+    {
+		return { s * v.x, s * v.y }; // z removido
+    }
+
+    template <typename real>
+    inline Vec2<real> // Tipo Vec2
+		operator *(const Vec2<real>& v, real s) // Tipo Vec2
+    {
+        return s * v;
+    }
+
+    template <typename real>
+    inline real
+        Vec<2, real>::length() const // Ajustado para 2 dimensões
+    {
+		return std::sqrt(x * x + y * y); // z removido
+    }
+
+    template <typename T>
+    std::ostream&
+        operator <<(std::ostream& os, const Vec2<T>& v) // Tipo Vec2
+    {
+        os << '(' << v.x << ',' << v.y << ')'; // z removido
+        return os;
+    }
+
+    using Vec2f = Vec2<float>; // Nome Vec2f e Tipo Vec2
+
+} // end namespace tcii::cg
+
+#endif // __Vec3_h
