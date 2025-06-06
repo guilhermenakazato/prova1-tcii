6afbd40812a0f446255c0fd343eaaa3b40aeb7c7
Felipe Jun <felipejun@outlook.com>
2025-06-05 22:39:04 -0300

    Main Utils

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,9 @@
 #include "ParticleSystem.h"
 #include "NameableObject.h"
 #include "Renderer.h"
+#include "Utils.h"
+#include "Vec2.h"
+#include "Bounds2.h"
 #include <cstdio>
 #include <iostream>
 
@@ -14,80 +17,121 @@ using namespace tcii::cg;
 int
 main()
 {
-  constexpr size_t D{3};
-  using R = float;
-  using A = PointVector<D, R>;
+	//constexpr size_t D{ 3 };
+	//using R = float;
+	//using A = PointVector<D, R>;
 
-  // criando uma KdTree com n pontos
-  constexpr size_t np{5};
-  KdTree<D, R, A> tree{prand<D, R>(np)};
-  auto& points = tree.points();
-  auto& bounds = tree.bounds();
+	//// criando uma KdTree com n pontos
+	//constexpr size_t np{ 5 };
+	//KdTree<D, R, A> tree{ prand<D, R>(np) };
+	//auto& points = tree.points();
+	//auto& bounds = tree.bounds();
 
-  std::cout.precision(3);
-  std::cout << "Tree bounds: " << bounds << '\n';
-  std::cout << "Points: " << points.size() << '\n';
+	//std::cout.precision(3);
+	//std::cout << "Tree bounds: " << bounds << '\n';
+	//std::cout << "Points: " << points.size() << '\n';
 
-  // pra q serve isso
-  R d{};
-  for (unsigned i = 0; i < np; ++i)
-    d += distance(points[i], bounds);
-  std::cout << "Distance sum: " << d << '\n';
+	//// pra q serve isso
+	//R d{};
+	//for (unsigned i = 0; i < np; ++i)
+	//	d += distance(points[i], bounds);
+	//std::cout << "Distance sum: " << d << '\n';
 
-  // n vizinhos do ponto no índice 0 e imprimindo aqueles em um raio 
-  (void)tree.findNeighbors(0, 3);
-  tree.forEachNeighbor(0, 
-   2.f,
-   [](const A& points, unsigned index)
-   {
-     std::cout << points[index] << '\n';
+	//// n vizinhos do ponto no índice 0 e imprimindo aqueles em um raio 
+	//(void)tree.findNeighbors(0, 3);
+	//tree.forEachNeighbor(0,
+	//	2.f,
+	//	[](const A& points, unsigned index)
+	//	{
+	//		std::cout << points[index] << '\n';
 
-     return true;
-   });
+	//		return true;
+	//	});
 
-  //auto ps = ParticleSystem<Vec3f>::New();
 
-  //ps->setParticleBuffer(1000);
+	///////////////////////////////////////////////////////Particle System
+	//auto ps = ParticleSystem<Vec3f>::New();
 
-  //auto pb = ps->particles();
+	//ps->setParticleBuffer(1000);
 
-  //pb->add(Vec3f{ 1.0f, 1.5f, 2.0f }, Vec3f{ 123.0f, 456.0f, 678.0f });
-  //pb->add(Vec3f{ 2.5f, 3.0f, 3.5f }, Vec3f(111.0f, 222.0f, 444.0f));
-  //pb->add(Vec3f{ 4.0f, 4.5f, 5.0f }, Vec3f(333.0f, 666.0f, 999.0f));
+	//auto pb = ps->particles();
 
-  //auto pc = pb->particleCount();
+	//pb->add(Vec3f{ 1.0f, 1.5f, 2.0f }, Vec3f{ 123.0f, 456.0f, 678.0f});
+	//pb->add(Vec3f{ 2.5f, 3.0f, 3.5f }, Vec3f(111.0f, 222.0f, 444.0f));
+	//pb->add(Vec3f{ 4.0f, 4.5f, 5.0f }, Vec3f(333.0f, 666.0f, 999.0f));
 
-  //printf("%s (capacity: %d)\n", ps->name(), pb->capacity());
-  //std::cout << "Particles: " << pc << "\nBounds: ";
-  //(pc ? std::cout << ps->bounds() : std::cout << "<empty>") << '\n';
+	//auto pc = pb->particleCount();
 
-  //ParticleArray<Vec3f> pa(*ps); // constructor
-  //std::cout << "Testing ParticleArray:\n";
-  //for (auto i = 0u; i < pa.size(); i++) // .size() and operator[]()
-  //{
-  //    std::cout << "Particle Position: " << std::get<0>(pa[i]) << " of RGB " << std::get<1>(pa[i]) << '\n'; // Talvez corrigir o jeito que chama
-  //}
+	//printf("%s (capacity: %d)\n", ps->name(), pb->capacity());
+	//std::cout << "Particles: " << pc << "\nBounds: ";
+	//(pc ? std::cout << ps->bounds() : std::cout << "<empty>") << '\n';
 
-  //Renderer renderer;
-  //ps->render(renderer);
+	//ParticleArray<Vec3f> pa(*ps); // constructor
+	//std::cout << "Testing ParticleArray:\n";
+	//for (auto i = 0u; i < pa.size(); i++) // .size() and operator[]()
+	//{
+	//	std::cout << "Particle Position: " << std::get<0>(pa[i]) << " of RGB " << std::get<1>(pa[i]) << '\n'; // Talvez corrigir o jeito que chama
+	//}
+	//std::cout << "Filter on particle 0 (should be true): " << filter(pa, 0, { 123.0f, 456.0f, 678.0f }) << std::endl;
+	//std::cout << "Filter on particle 1 (should be false): " << filter(pa, 1, { 123.0f, 456.0f, 678.0f }) << std::endl;
 
-	// KNN<float> heap{7};
-  //   heap.add(100.0f, 0);
-	// heap.add(50.0f, 1);
-	// heap.add(75.0f, 4);
-	// heap.add(25.0f, 6);
-	// heap.add(125.0f, 2);
-	// heap.add(200.0f, 5);
-	// heap.add(150.0f, 3);
+	//for (size_t i = 0; i < 7; ++i) 
+	//{
+	//		auto pair = aux[i];
+	//	std::cout << "KNN[" << i << "] = " << pair.first << " " << pair.second << '\n';
+	//}
 
-	// auto aux = heap.neighbors();
+	//Renderer renderer;
+	//ps->render(renderer);
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	// for (size_t i = 0; i < 7; ++i) {
-	// 	auto pair = aux[i];
-	//     std::cout << "KNN[" << i << "] = " << pair.first << " " << pair.second << '\n';
-	// }
-		
-  puts("Press any key to exit...");
-  (void)getchar();
-  return 0;
+	/////////////////////////////////////////////////////// Heap
+	  // KNN<float> heap{7};
+	  //   heap.add(100.0f, 0);
+	  // heap.add(50.0f, 1);
+	  // heap.add(75.0f, 4);
+	  // heap.add(25.0f, 6);
+	  // heap.add(125.0f, 2);
+	  // heap.add(200.0f, 5);
+	  // heap.add(150.0f, 3);
+
+	  // auto aux = heap.neighbors();
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	/////////////////////////////////////////////////////// Vec2 Bounds 2
+	//std::cout << "===Vec2===\n";
+	//Vec2f v2a{ 1.0f, 2.0f };
+	//Vec2f v2b{ 3.0f, 4.0f };
+	//Vec2f v2c = v2a + v2b;
+	//Vec2f v2d = v2b - v2a;
+	//Vec2f v2e = 2.0f * v2a;
+	//Vec2f v2f = v2a * 3.0f;
+	//float v2len = v2a.length();
+
+	//std::cout << "[(1,2)]Vec2f v2a: " << v2a << std::endl;
+	//std::cout << "[(3,4)]Vec2f v2b: " << v2b << std::endl;
+	//std::cout << "[(4,6)]v2a + v2b: " << v2c << std::endl;
+	//std::cout << "[(2,2)]v2b - v2a: " << v2d << std::endl;
+	//std::cout << "[(2,4)]2.0 * v2a: " << v2e << std::endl;
+	//std::cout << "[(3,6)]v2a * 3.0: " << v2f << std::endl;
+	//std::cout << "[(2.63067)]v2a.length(): " << v2len << std::endl;
+	//std::cout << "[(true)]v2a == v2a: " << (v2a == v2a) << std::endl;
+	//std::cout << "[(false)]v2a == v2b: " << (v2a == v2b) << std::endl;
+
+	//std::cout << "\n===Bounds2===\n";
+	//Bounds2f b2;
+	//b2.setEmpty();
+	//b2.inflate(v2a);
+	//b2.inflate(v2b);
+	//std::cout << "[(1,2) (3,4)]inflate(v2a) inflate(v2b)" << b2 << std::endl;
+	//std::cout << "[1,2]min(): " << b2.min() << std::endl;
+	//std::cout << "[3,4]max(): " << b2.max() << std::endl;
+	//std::cout << "[true]contains(1,2): " << b2.contains(v2a) << std::endl;
+	//std::cout << "[false]contains(Vec2f{0.0f, 0.0f}): " << b2.contains(Vec2f{ 0.0f, 0.0f }) << std::endl;
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	puts("Press any key to exit...");
+	(void)getchar();
+	return 0;
 }
--- Utils.h
+++ Utils.h
@@ -3,97 +3,135 @@
 
 #include "Bounds3.h"
 #include "Concepts.h"
+#include "ParticleArray.h"
 #include <vector>
 
 namespace tcii::p1
 { // begin namespace tcii::p1
-using namespace cg;
+    using namespace cg;
+    using namespace tcii::physx;
 
-// apenas calculando os limites da caixa usando todos os pontos recebidos
-template <size_t D, IsReal R, IsPointArray<Vec, R, D> A>
-auto
-computeBounds(const A& points)
-{
-  Bounds<D, R> b;
+    // apenas calculando os limites da caixa usando todos os pontos recebidos
+    template <size_t D, IsReal R, IsPointArray<Vec, R, D> A>
+    auto
+        computeBounds(const A& points)
+    {
+        Bounds<D, R> b;
 
-  for (size_t n = points.size(), i = 0; i < n; ++i)
-    b.inflate(points[i]);
-  return b;
-}
+        for (size_t n = points.size(), i = 0; i < n; ++i)
+            b.inflate(points[i]);
+        return b;
+    }
 
-template <size_t D, IsReal R>
-auto
-distance(const Vec<D, R>& p, const Bounds<D, R>& bounds)
-{
-  R d{};
+    <<<<<< < HEAD
+        template <size_t D, IsReal R>
+    auto
+        distance(const Vec<D, R>&p, const Bounds<D, R>&bounds)
+    {
+        R d{};
+        ====== =
+            template <size_t D, IsReal R>
+        auto
+            distance(const Vec<D, R>&p, const Bounds<D, R>&bounds)
+        {
+            R d{};
+            >>>>>> > 8ff540dd2bbfa4457e3d71d138638457c28bf90d
 
-  for (size_t i = 0; i < D; ++i)
-    if (auto t = p[i] - bounds[0][i]; t < 0)
-      d += t * t;
-    else if (auto t = p[i] - bounds[1][i]; t > 0)
-      d += t * t;
-  return (R)std::sqrt(d);
-}
+                for (size_t i = 0; i < D; ++i)
+                    if (auto t = p[i] - bounds[0][i]; t < 0)
+                        d += t * t;
+                    else if (auto t = p[i] - bounds[1][i]; t > 0)
+                        d += t * t;
+            return (R)std::sqrt(d);
+        }
 
-template<size_t D, IsReal R>
-auto 
-distance(const Vec<D,R>& a, const Vec<D,R>& b) {
-  R d{};
+        <<<<<< < HEAD
+            template<size_t D, IsReal R>
+        auto
+            distance(const Vec<D, R>&a, const Vec<D, R>&b) {
+            R d{};
+            ====== =
+                template<size_t D, IsReal R>
+            auto
+                distance(const Vec<D, R>&a, const Vec<D, R>&b) {
+                R d{};
 
-  for (size_t i = 0; i < D; ++i)
-    d += std::pow(a[i] - b[i], 2);
-  
-  return (R)std::sqrt(d);
-}
+                for (size_t i = 0; i < D; ++i)
+                    d += std::pow(a[i] - b[i], 2);
 
-// retorna um número real entre 0 e 1 
-template <IsReal R>
-inline auto
-srand()
-{
-  return (R)((double)rand() / RAND_MAX);
-}
+                return (R)std::sqrt(d);
+            }
 
-// retorna um número entre min e max
-template <IsReal R>
-inline auto
-srand(R min, R max)
-{
-  return min + srand<R>() * (max - min);
-}
+            // retorna um número real entre 0 e 1 
+            template <IsReal R>
+            inline auto
+                srand()
+            {
+                return (R)((double)rand() / RAND_MAX);
+            }
+            >>>>>> > 8ff540dd2bbfa4457e3d71d138638457c28bf90d
 
-// retorna um ponto de dimensão D e precisão R com os valores de D (x,y,z,....) estando entre min e max
-template <size_t D, IsReal R>
-inline auto
-prand(R min, R max)
-{
-  Vec<D, R> p;
+                for (size_t i = 0; i < D; ++i)
+                    d += std::pow(a[i] - b[i], 2);
 
-  for (size_t i = 0; i < D; ++i)
-    p[i] = srand(min, max);
-  return p;
-}
+            return (R)std::sqrt(d);
+        }
 
-template <size_t D, IsReal R>
-using PointVector = std::vector<Vec<D, R>>;
+        // retorna um número real entre 0 e 1 
+        template <IsReal R>
+        inline auto
+            srand()
+        {
+            return (R)((double)rand() / RAND_MAX);
+        }
 
-// retorna um vetor com pontos de dimensão D e precisão R com os valores de D (x,y,z,....) estando entre min e max
-template <size_t D, IsReal R>
-inline auto
-prand(size_t n, R min = 0, R max = 1)
-{
-  assert(n > 0);
+        // retorna um número entre min e max
+        template <IsReal R>
+        inline auto
+            srand(R min, R max)
+        {
+            return min + srand<R>() * (max - min);
+        }
 
-  PointVector<D, R> v(n);
+        // retorna um ponto de dimensão D e precisão R com os valores de D (x,y,z,....) estando entre min e max
+        template <size_t D, IsReal R>
+        inline auto
+            prand(R min, R max)
+        {
+            Vec<D, R> p;
 
-  for (size_t i = 0; i < n; ++i)
-    v[i] = prand<D>(min, max);
-  return v;
-}
+            for (size_t i = 0; i < D; ++i)
+                p[i] = srand(min, max);
+            return p;
+        }
 
-// especialização pra 3D
-template <typename R> using Points3 = PointVector<3, R>;
+        template <size_t D, IsReal R>
+        using PointVector = std::vector<Vec<D, R>>;
 
-} // end namespace tcii::p1
+        // retorna um vetor com pontos de dimensão D e precisão R com os valores de D (x,y,z,....) estando entre min e max
+        template <size_t D, IsReal R>
+        inline auto
+            prand(size_t n, R min = 0, R max = 1)
+        {
+            assert(n > 0);
+
+            PointVector<D, R> v(n);
+
+            for (size_t i = 0; i < n; ++i)
+                v[i] = prand<D>(min, max);
+            return v;
+        }
+
+        // filter(Pontos<coord, cor>, i, cor) -> bool
+        bool
+            filter(const ParticleArray<Vec3f>&A, unsigned i, Vec3f C)
+        {
+            return std::get<1>(A[i]) == C;
+        }
+
+        // especialização pra 3D
+        template <typename R> using Points3 = PointVector<3, R>;
+
+    } // end namespace tcii::p1
 
 #endif // __Utils_h
